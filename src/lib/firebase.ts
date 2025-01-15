19d8d405d9a0b58c647d421f14276c2ab055286e
André Júnior <atipicode.ec@gmail.com>
2025-01-15 12:02:29 -0300

    arth-birth fixed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,17 @@
 import admin, { ServiceAccount } from 'firebase-admin'
-import serviceAccount from './firebase-credentials.json'
 
-if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as ServiceAccount),
-    })
+const firebaseCredentialsBase64 = process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS;
+
+if (!firebaseCredentialsBase64) {
+  throw new Error('A variável de ambiente FIREBASE_CREDENTIALS_BASE64 não foi definida');
 }
 
-export const db = admin.firestore()
+const serviceAccount = JSON.parse(Buffer.from(firebaseCredentialsBase64, 'base64').toString('utf-8'));
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  });
+}
+
+export const db = admin.firestore();
